05cde537041ab22a9935681c3035b309bde60d50
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-31 12:24:44 +0530

    Testing '

--- app/components/Components.tsx
+++ app/components/Components.tsx
@@ -22,10 +22,9 @@ function Twitter() {
         <div className="content tweet flex flex-col lg:flex-row lg:items-center justify-between">
         <div className="flex items-center justify-between mb-2 lg:mb-0">
   <div className="flex items-center">
-    <Image
-      src="https://pbs.twImage.com/profile_images/1666401418104934402/2YENIgPj_400x400.jpg"
+    <img
+      src="https://pbs.twimg.com/profile_images/1666401418104934402/2YENIgPj_400x400.jpg"
       className="rounded-full h-12 w-12 mr-2 lg:mr-4"
-      alt="Twitter"
     />
     <div className="flex flex-col">
       <span className="text-sm">Sushil Pandey</span>
@@ -33,10 +32,9 @@ function Twitter() {
     </div>
   </div>
   <div className="flex items-center pl-10">
-    <Image
+    <img
       src="https://uploads-ssl.webflow.com/62c89bdb7c26b515f632de67/630d83f5a3b284176ffe8b1b_twitter-icon-min.png"
       className=" rounded-full h-12 w-12 bg-sky-400"
-      alt="Twitter icon"
     />
   </div>
 </div>
@@ -48,7 +46,7 @@ function Twitter() {
         <div className="content tweet flex justify-center">
           <a href="https://twitter.com/contactsushill" className="flex rounded-lg border p-1 m-2">
             <span>Tweets</span>
-            <Image
+            <img
               src="https://uploads-ssl.webflow.com/62c89bdb7c26b515f632de67/62ca186ae691b25b1768cbfe_arrow-angle.svg"
               alt=""
               className="h-4 w-4 ml-1"
@@ -66,8 +64,8 @@ function Description(){
     return (
         <>
             <h1 className="aboutsh font-serif-variation font-serif text-2xl font-light !leading-tight text-neutral-400 sm:text-3xl lg:text-4xl">Hey there, I’m <span className="blackpp">Sushil</span>👋 Welcome to my corner of the internet 🥹 I have a knack for crafting digital <span className="blackpp">products</span> and currently contribute to the innovative team at <span className="blackpp">Filos</span>.<br/><br/>
-            When I'm not delving into pixels and code, you'll often catch me diving into the vibrant rhythms of Indian hip hop, beats and lyrics.<br/><br/>
-            I'm not a serious <span className="blackpp">writer</span>, but I enjoy dedicating my free time to putting my thoughts into words.</h1><br/><br/>
+            When Im not delving into pixels and code, you&#39;ll often catch me diving into the vibrant rhythms of Indian hip hop, beats and lyrics.<br/><br/>
+            I&#39;m not a serious <span className="blackpp">writer</span>, but I enjoy dedicating my free time to putting my thoughts into words.</h1><br/><br/>
         
         </>
     )
@@ -105,7 +103,7 @@ function IhhComp() {
                 Side="IHH Player"
             />
             <div className="boxdlee grow overflow-hidden transition-colors focus-within:bg-neutral-300 hover:bg-neutral-300 ">
-                <Image className="imageihh" src="https://res.cloudinary.com/djug8zfu7/image/upload/v1710139256/x9ziaczvderovrbyd39p.png" alt="IHH Player"/>
+                <img className="imageihh" src="https://res.cloudinary.com/djug8zfu7/image/upload/v1710139256/x9ziaczvderovrbyd39p.png" alt="IHH Player"/>
             </div>
         </div>
     );
@@ -124,7 +122,7 @@ function FasciaComp() {
                 Side="IHH Player"
             />
             <div className={`relative overflow-hidden transition-transform px-2 duration-500 ${hovered ? '' : 'translate-y-6'}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-                <Image src="https://camo.githubusercontent.com/f1e17d3bd0b19438a0c6a68182006984dda52089ce0f0592eaabc4f74feae236/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f646a7567387a6675372f696d6167652f75706c6f61642f665f6175746f2c715f6175746f2f757a6f767671396173396f327a65736d34767a71" alt="IHH Player" style={{ position: 'relative', top: hovered ? '10px' : '30px' }} />
+                <img src="https://camo.githubusercontent.com/f1e17d3bd0b19438a0c6a68182006984dda52089ce0f0592eaabc4f74feae236/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f646a7567387a6675372f696d6167652f75706c6f61642f665f6175746f2c715f6175746f2f757a6f767671396173396f327a65736d34767a71" alt="IHH Player" style={{ position: 'relative', top: hovered ? '10px' : '30px' }} />
             </div>
         </div>
     );
